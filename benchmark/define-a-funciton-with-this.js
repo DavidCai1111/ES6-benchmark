9dd87cff473f1d7328f9ac2e5a9ff4ee211f05fe
Julien CROUZET <jcrouzet.actemaconsulting@tf1.fr>
2016-03-31 14:21:32 +0200

    Add a bind() bench to compare perf gain

--- benchmark/define-a-funciton-with-this.js
+++ benchmark/define-a-funciton-with-this.js
@@ -1,6 +1,6 @@
 'use strict'
 suite('define a function with inherited this', function () {
-  bench('function statement', function () {
+  bench('function statement with self = this', function () {
     var self = this
     var a = function () {
       self
@@ -9,8 +9,16 @@ suite('define a function with inherited this', function () {
 
     a();
   })
+  bench('function statement with bind', function () {
+    var a = function () {
+      this
+      return '1'
+    }.bind(this);
 
-  bench('() =>', function () {
+    a();
+  })
+
+  bench('arrow function () =>', function () {
     var a = () => {
       this
       return '1'
